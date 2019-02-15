26b2ee4c289b38db7259755ee366bca6b98630a1
Kristel Fung <kristelfung@gmail.com>
2019-02-15 09:20:55 -0500

    add backgrounds and fonts

--- src/Hackathon.js
+++ src/Hackathon.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Hackathon = (props) => {
     return (
-        <div class="hackathon">
-            <img src={props.data.imageUrl} className="hackathon__image"/>
-            <p>{props.data.name}</p>
+        <div className="hackathon">
+            <div className="hackathon__background">
+                <img src={props.data.imageUrl} className="hackathon__image"/>
+                <p>{props.data.name}</p>
+            </div>
         </div>
     )
 }
