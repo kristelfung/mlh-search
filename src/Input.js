eb2bf2295a6c69a1a003b3ebb1098d577c837903
Kristel Fung <kristelfung@gmail.com>
2019-02-21 21:08:19 -0500

    add search icon

--- src/Input.js
+++ src/Input.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import searchIcon from './images/search.svg';
 
 const Input = (props) => {
     return (
         <div>
             <form className="search">
+                <img src={searchIcon} className="icon"/>
                 <input type="text" 
                     placeholder={props.placeholder} 
                     onChange={(e) => props.filterText(e.target.value)}
