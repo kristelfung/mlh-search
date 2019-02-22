4102e9b857cfbc951e94ad1e50e23e0dd0b58d37
Kristel Fung <kristelfung@gmail.com>
2019-02-21 20:51:31 -0500

    add hackathon links

--- src/Hackathon.js
+++ src/Hackathon.js
@@ -7,16 +7,18 @@ const convertDate = (date) => {
 const Hackathon = (props) => {
     return (
         <div className="hackathon">
-            <div className="hackathon__background">
-                <img src={props.data.imageUrl} className="hackathon__image"/>
-                <div className="hackathon__text">
-                    <h2>{props.data.name}</h2>
-                    <p className="hackathon__date">
-                        {convertDate(props.data.startDate)}
-                    </p>
-                    <p>{props.data.location}</p>
+            <a href={props.data.url} className="hackathon__link">
+                <div className="hackathon__background">
+                    <img src={props.data.imageUrl} className="hackathon__image"/>
+                    <div className="hackathon__text">
+                        <h2>{props.data.name}</h2>
+                        <p className="hackathon__date">
+                            {convertDate(props.data.startDate)}
+                        </p>
+                        <p>{props.data.location}</p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
     )
 }
